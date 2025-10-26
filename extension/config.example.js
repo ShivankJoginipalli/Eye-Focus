d839496d1bba9d2821938163845a849878284762
adnansanaulla <adnansanaulla7860@gmail.com>
2025-10-26 00:40:31 -0400

    changed config setup

--- extension/config.example.js
+++
@@ -1,6 +0,0 @@
-// API Configuration Template
-// Copy this file to config.js and add your API keys
-
-const API_CONFIG = {
-  groqKey: 'YOUR_GROQ_API_KEY_HERE'  // Get from https://console.groq.com/keys
-};
